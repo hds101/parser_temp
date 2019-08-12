fe6fdac1d21765b2d1dc593aed8486ed39102d3a
Dmitry Y <hds101@gmail.com>
2019-08-12 12:07:39 +0300

    Restore and fix Gust parser

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,11 @@
 FROM ruby:2.6.3
 
-RUN apt-get update -qq && apt-get install -q -y lsof unzip wget tar openssl xvfb chromium # firefox-esr
+RUN apt-get update -qq && apt-get install -q -y lsof unzip wget tar openssl xvfb chromium
 
 RUN cd /tmp && \
     wget https://chromedriver.storage.googleapis.com/2.44/chromedriver_linux64.zip && \
     unzip chromedriver_linux64.zip -d /usr/local/bin && \
     rm -f chromedriver_linux64.zip
-#
-#RUN cd /tmp && \
-#    wget https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz && \
-#    tar -xvzf geckodriver-v0.23.0-linux64.tar.gz -C /usr/local/bin && \
-#    rm -f geckodriver-v0.23.0-linux64.tar.gz
-#
-#RUN apt-get install -q -y chrpath libxft-dev libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev && \
-#    cd /tmp && \
-#    wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 && \
-#    tar -xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2 && \
-#    mv phantomjs-2.1.1-linux-x86_64 /usr/local/lib && \
-#    ln -s /usr/local/lib/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin && \
-#    rm -f phantomjs-2.1.1-linux-x86_64.tar.bz2
 
 RUN mkdir -p /app
 WORKDIR /app
@@ -30,7 +17,5 @@ RUN gem install bundler
 RUN bundle install
 
 COPY . /app
-
-EXPOSE 80
 
 CMD ["ruby", "app.rb"]
--- lib/gust_spider.rb
+++ lib/gust_spider.rb
@@ -1,26 +1,103 @@
 class GustSpider < Kimurai::Base
   @name = "gust_spider"
   @engine = :selenium_chrome
-  @start_urls = ["https://infinite-scroll.com/demo/full-page/"]
+  @start_urls = ["https://gust.com/search/new?category=startups&page=1&partial=results"]
+  @config = {
+      user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
+      before_request: { delay: 1..3 }
+  }
+
+  # TODO: redis state: current_page/status
 
   def parse(response, url:, data: {})
-    posts_headers_path = "//article/h2"
-    count = response.xpath(posts_headers_path).count
+    response.xpath("//div[@id='search_results']//li[@class='list-group-item']//div[@class='card-title']/a")
+            .each do |company_link|
 
-    loop do
-      browser.execute_script("window.scrollBy(0,10000)") ; sleep 2
-      response = browser.current_response
+      company = { name: company_link.text,
+                  href: company_link[:href] }
 
-      new_count = response.xpath(posts_headers_path).count
-      if count == new_count
-        logger.info "> Pagination is done" and break
-      else
-        count = new_count
-        logger.info "> Continue scrolling, current count is #{count}..."
+      request_to :parse_company_page, url: absolute_url(company[:href], base: url), data: company
+    end
+
+
+    index = /page=(\d+)/.match(url)[1].to_i
+    return if index == 2
+
+    next_page = response.at_xpath("//div[@id='search_results']//li[@class='last']/a")
+    request_to :parse, url: absolute_url(next_page[:href], base: url) if next_page
+  end
+
+  def parse_company_page(response, url:, data: {})
+    logger.debug "=========="
+    logger.debug "Parsing #{ data[:name] } at #{ data[:href] }"
+
+    company = fetch_company(response).merge(parsed_at: Time.now,
+                                            users: fetch_users(response))
+
+    logger.debug company.to_s
+    logger.debug "=========="
+  end
+
+  private
+
+  def tag_by_section(section)
+    case section
+    when 'management' then 'team'
+    when 'advisors'   then 'advisors'
+    when 'investor'   then 'previous_investors'
+    else nil
+    end
+  end
+
+  def fetch_company(response)
+    company = {
+      name: response.at_xpath("//div[@id='company_info']//h2")&.text,
+      slogan: response.at_xpath("//div[@id='company_info']//p[contains(@class, 'quote')]")&.text,
+      overview: response.at_xpath("//div[@id='company_overview']//div[contains(@class, 'panel-body')]/p")&.text
+    }
+
+    list_xpath = "//div[@id='company_info']//ul/li[@class='list-group-item']"
+    response.xpath(list_xpath).each do |item|
+      column = item.text.split[0].downcase.to_sym
+      value  = item.at_xpath("./span[contains(@class, 'value')]")&.text
+      case column
+      when :website then value = item.at_xpath("./span[contains(@class, 'value')]/a")[:href]
+      when :incorporation then column = :incorporation_type
+      else nil
+      end
+      company[column] = value
+    end
+    company.transform_values { |v| v&.strip }
+  end
+
+  def fetch_users(response)
+    users = []
+
+    team_xpath = "//div[@id='management']//li[contains(@class, 'list-group-item')]//div[@class='row']"
+    response.xpath(team_xpath).each do |user|
+      item = {}
+      user_title = user.at_xpath(".//div[contains(@class, 'card')]//div[@class='card-title']")
+      user_link = user.at_xpath(".//div[contains(@class, 'card')]//div[@class='card-title']/a")
+      item[:href] = user_link&.send(:[], :href)
+      item[:name] = user_link&.text || user_title&.text
+      item[:role] = user.at_xpath(".//div[contains(@class, 'card')]//div[@class='card-subtitle']")&.text
+      item[:description] = user.at_xpath("./div[@class='col-md-7']")&.text
+      item[:tag] = tag_by_section('management')
+      users << item.transform_values { |v| v&.strip }
+    end
+
+    %w(advisors investor).each do |section|
+      card_xpath = "//div[@id='#{section}']//li[contains(@class, 'list-group-item')]//div[@class='card-title']"
+      response.xpath(card_xpath).each do |user|
+        item = {}
+        user_link = user.at_xpath("./a")
+        item[:href] = user_link&.send(:[], :href)
+        item[:name] = user_link&.text || user&.text
+        item[:tag] = tag_by_section(section)
+        users << item.transform_values { |v| v&.strip }
       end
     end
 
-    posts_headers = response.xpath(posts_headers_path).map(&:text)
-    logger.info "> All posts from page: #{posts_headers.join('; ')}"
+    users
   end
 end
